b42499ba683e1c67e08867fb8162cc805fca46fc
miguelvaldmart <miguelvaldmart@gmail.com>
2025-11-16 23:18:45 -0600

    Modified CoinBehaviourTests

--- AvatarsVSRooksGame/Assets/Tests/CoinBehaviourTests.cs
+++ AvatarsVSRooksGame/Assets/Tests/CoinBehaviourTests.cs
@@ -32,20 +32,6 @@ public class CoinManagerTests
         CoinManager.Instance = null;
     }
     
-    [Test]
-    public void Test1AddCoins()
-    {
-        // Arrange: Comenzar con 100 monedas
-        int initialCoins = coinManager.GetTotalCoins();
-        int coinsToAdd = 25;
-
-        // Act: Agregar 25 monedas
-        coinManager.AddCoins(coinsToAdd);
-
-        // Assert: Verificar que ahora tenga 125 monedas
-        Assert.AreEqual(initialCoins + coinsToAdd, coinManager.GetTotalCoins(),
-            "Las monedas no se sumaron correctamente");
-    }
 
     [Test]
     public void Test2AddCoins()
